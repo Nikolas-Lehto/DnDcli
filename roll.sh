b4fb9bfe116bae2ade170517c6adb7fa3f5e623c
LiquidDenton <kristo@kristoserver.lan>
2022-10-28 15:40:48 +0000

    modified:   roll.sh

--- roll.sh
+++ roll.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-let "roll = 1 + $RANDOM % $1"
-echo $roll
+let "dice = $1"
+let "mod = $2"
+
+if [$3 != -A] && [$3 != -D] #puuttuu kuulemma tästä ']'. kato jos osaisit fixata
+then
+	let "roll = $mod + 1 + $RANDOM % $dice"
+	echo $roll
+fi
+
+
+#aloin siis rakentaa tätä modifier ja advantage systeemii
+#fixaile rauhassa :D
